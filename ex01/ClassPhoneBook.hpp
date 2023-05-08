106b33de5a986c0460679ace1b6aa46fe49aef50
meritissimo1 <marcelomoreira1905@gmail.com>
2023-05-08 18:16:13 -0300

    menu created :)

--- ex01/ClassPhoneBook.hpp
+++ ex01/ClassPhoneBook.hpp
@@ -3,6 +3,7 @@
 
 # include <iostream>
 # include <string.h>
+# include <iomanip>
 
 class PhoneBook {
 
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,18 +1,37 @@
 #include "ClassPhoneBook.hpp"
 
+void	menu( void )
+{
+	std::cout << std::left << std::setfill('.') << std::setw(12);
+	std::cout << "ADD" << "Save a new contact" << std::endl;
+	std::cout << std::left << std::setfill('.') << std::setw(12);
+	std::cout << "SEARCH" << "Display a specific contact" << std::endl;
+	std::cout << std::left << std::setfill('.') << std::setw(12);
+	std::cout << "EXIT" << "Close the program" << std::endl;
+}
+
+
 int	main()
 {
 	PhoneBook	instance;
 	
 	char	buffer[42];
 
-	while (strcmp(buffer, "EXIT"))
+	while (1)
 	{
-		
-		std::cout << "" << std::endl;
+		menu();
 		std::cin >> buffer;
-		
-	}
-
+		if (!strcmp(buffer, "EXIT"))
+		{
+			char	decision;
+			std::cout << "All the contacts will lost, are you sure about that? [Y/N]";
+			std::cin >> decision;
+			if (decision == 'y' || decision == 'Y')
+			{
+				std::cout  << "B" << decision << "e" << std::endl;
+				return (0);
+			}
+		}
+	}	 
 	return (0);
 }
