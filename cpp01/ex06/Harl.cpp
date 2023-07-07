0d29833a8197569cfc69b902a2cb3a7f1fbcdc9f
meritissimo1 <marcelomoreira1905@gmail.com>
2023-07-07 12:41:07 -0300

    add cpp03; fixing bug with commit

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -29,11 +29,7 @@ void	Harl::complain(std::string level)
 	int	value;
 	int	i;
 
-<<<<<<< HEAD
-	value = 0;
-=======
 	value = 4;
->>>>>>> 2cad22e9b4a7bdca2cdf01d9329147d0da0976fd
 	static const  enumMap levels[] = {
 		{"DEBUG", DEBUG},
 		{"INFO", INFO},
@@ -48,25 +44,6 @@ void	Harl::complain(std::string level)
 			break;
 		}
 	}
-<<<<<<< HEAD
-	if (i < 4)
-	{
-		switch(value)
-		{
-			case DEBUG:
-				this->debug();
-			case INFO:
-				this->info();
-			case WARNING:
-				this->warning();
-			case ERROR:
-				this->error();
-		}
-	
-	}
-	else
-		std::cout << "error: Not match" << std::endl;
-=======
 	switch(value)
 	{
 		case DEBUG:
@@ -81,6 +58,5 @@ void	Harl::complain(std::string level)
 		default:
 			std::cout << "error: Not match" << std::endl;
 	}	
->>>>>>> 2cad22e9b4a7bdca2cdf01d9329147d0da0976fd
 }
 
