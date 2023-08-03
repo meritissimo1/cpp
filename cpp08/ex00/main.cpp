63f3d8226b13d68ae466e35adf072a7b27ec21b4
meritissimo1 <marcelomoreira1905@gmail.com>
2023-08-03 16:56:27 +0200

    some test tweaks

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -3,13 +3,14 @@
 int main ( void )
 {
 	{
+		std::cout << "\n----- Container list -----\n";
 		std::list<int>	list1;
 
 		list1.push_back(42);
-		list1.push_back(37);  
-		list1.push_back(10);  
-		list1.push_back(-1);  
-		list1.push_back(5);  
+		list1.push_back(37);
+		list1.push_back(10);
+		list1.push_back(-1);
+		list1.push_back(5);
 	
 		try
 		{
@@ -27,6 +28,7 @@ int main ( void )
 	}
 	
 	{
+		std::cout << "\n----- Container vector -----\n";
 		std::vector<int> vec;
 		vec.push_back(1);
 		vec.push_back(2);
